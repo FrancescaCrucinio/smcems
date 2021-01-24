f05e1edcb16823bf2cbf4fb38bff79341d91f492
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2021-01-24 11:07:17 +0000

    comparison with DKDE

--- gaussian_mixture/mixture_mise_vs_runtime.m
+++ gaussian_mixture/mixture_mise_vs_runtime.m
@@ -10,13 +10,13 @@ h = @(y) 2*normpdf(y, 0.3, sqrt(0.043^2 + 0.045^2))./3 + ...
 g = @(x,y) normpdf(y, x, 0.045);
 f = @(x) normpdf(x, 0.3, 0.015)/3 + normpdf(x, 0.5, 0.043)*2/3;
 
+sigG = 0.045;
+varG = sigG^2;
 % set paremeters
 % number of iterations
 Niter = 100;
 % number of bins/particles
 N = [100, 500, 1000, 5000, 10000];
-% number of samples for IB
-nsamples = 1000;
 % scale for SMC smoothing
 epsilon = 1e-03;
 % smoothing kernel
@@ -27,14 +27,21 @@ Nrep = 1000;
 % execution times
 EMStime = zeros(Nrep, length(N));
 SMCtime = zeros(Nrep, length(N));
+DKDEpitime = zeros(Nrep, length(N));
+DKDEcvtime = zeros(Nrep, length(N));
 % mise
 EMSstats = zeros(Nrep, length(N));
 SMCstats = zeros(Nrep, length(N));
+DKDEpistats = zeros(Nrep, length(N));
+DKDEcvstats = zeros(Nrep, length(N));
 
 parfor index=1:length(N)
     for k=1:Nrep
+        % sample from h
+        y = Ysample_gaussian_mixture(N(index));
         % bin centers
         fBin = 1/(2*N(index)):1/N(index):1;
+        dx = fBin(2) - fBin(1);
         % discretisation of h
         hDisc = h(fBin);
         % discretisation of g
@@ -50,7 +57,7 @@ parfor index=1:length(N)
         % SMC
         tstart = tic;
         [x, W] = smc_gaussian_mixture(N(index), Niter, epsilon,...
-            f0SMC);
+            f0SMC, y);
         SMCtime(k, index) = toc(tstart);
         % KDE
     	% bandwidth
@@ -67,29 +74,57 @@ parfor index=1:length(N)
         EMSres = ems(gDisc, hDisc, Niter, Kmatrix, f0EM);
         EMStime(k, index) = toc(tstart);
         [~, ~, ~, EMSstats(k, index)] = diagnosticsF(f, fBin, EMSres(Niter, :));
+        % DKDE PI
+        % PI bandwidth of Delaigle and Gijbels
+        tstart = tic;
+        hPI = PI_deconvUknownth4(y, 'norm', varG, sigG);
+        fdecPI = fdecUknown(fBin, y, hPI, 'norm', sigG, dx);
+        DKDEpitime(k, index) = toc(tstart);
+        [~, ~, ~, DKDEpistats(k, index)] = diagnosticsF(f, fBin, fdecPI);
+        % DKDE CV
+        % CV bandwidth of Stefanski and Carroll
+        tstart = tic;
+        hCV = CVdeconv(y, 'norm', sigG);
+        fdecCV = fdecUknown(fBin, y, hCV, 'norm', sigG, dx);
+        DKDEcvtime(k, index) = toc(tstart);
+        [~, ~, ~, DKDEcvstats(k, index)] = diagnosticsF(f, fBin, fdecCV);
     end
 end
 
 SMCruntime = mean(SMCtime, 1);
 EMSruntime = mean(EMStime, 1);
+DKDEpiruntime = mean(DKDEpitime, 1);
+DKDEcvruntime = mean(DKDEcvtime, 1);
 SMCmise = mean(SMCstats, 1);
 EMSmise = mean(EMSstats, 1);
+DKDEpimise = mean(DKDEpistats, 1);
+DKDEcvmise = mean(DKDEcvstats, 1);
 
 close all;
 h = zeros(2 + length(N),1);
-h(1) = semilogx(SMCruntime, SMCmise, ':b', 'LineWidth', 3);
+h(1) = semilogx(SMCruntime, SMCmise, ':', 'color', [0, 0.4470, 0.7410], 'LineWidth', 3);
 hold on
-h(2) = semilogx(EMSruntime, EMSmise, '-.r', 'LineWidth', 3);
+h(2) = semilogx(EMSruntime, EMSmise, '-.', 'color', [0.8500, 0.3250, 0.0980], 'LineWidth', 3);
+h(3) = semilogx(DKDEpiruntime, DKDEpimise, '-', 'color', [0.9290, 0.6940, 0.1250], 'LineWidth', 3);
+h(4) = semilogx(DKDEcvruntime, DKDEcvmise, '--', 'color', [0.4940, 0.1840, 0.5560], 'LineWidth', 3);
 markers = {'o' 's' 'd' 'x' 'p'};
 for i=1:length(N)
-    h(2 + i) = semilogx(SMCruntime(i), SMCmise(i), strcat(markers{i} ,'k'),...
+    h(4 + i) = semilogx(SMCruntime(i), SMCmise(i), strcat(markers{i}, 'k'),...
         'MarkerFaceColor', 'k', 'LineWidth', 4);
-    semilogx(SMCruntime(i), SMCmise(i), strcat(markers{i} ,'b'),...
-        'MarkerFaceColor', 'b', 'LineWidth', 4)
-    semilogx(EMSruntime(i), EMSmise(i), strcat(markers{i} ,'r'),...
-        'MarkerFaceColor', 'r', 'LineWidth', 4)
+    l = semilogx(SMCruntime(i), SMCmise(i), markers{i},...
+        'color', [0, 0.4470, 0.7410], 'LineWidth', 4);
+    l.MarkerFaceColor = l.Color;
+    l = semilogx(EMSruntime(i), EMSmise(i), markers{i},...
+        'color', [0.8500, 0.3250, 0.0980], 'LineWidth', 4);
+    l.MarkerFaceColor = l.Color;
+    l = semilogx(DKDEpiruntime(i), DKDEpimise(i), markers{i},...
+        'color', [0.9290, 0.6940, 0.1250], 'LineWidth', 4);
+    l.MarkerFaceColor = l.Color;
+    l = semilogx(DKDEcvruntime(i), DKDEcvmise(i), markers{i},...
+        'color', [0.4940, 0.1840, 0.5560], 'LineWidth', 4);
+    l.MarkerFaceColor = l.Color;
 end
-legend(h, 'SMC', 'EMS', ['N = ' num2str(N(1))], ['N = ' num2str(N(2))], ...
+legend(h, 'SMC', 'EMS', 'DKDE-pi', 'DKDE-cv', ['N = ' num2str(N(1))], ['N = ' num2str(N(2))], ...
     ['N = ' num2str(N(3))], ['N = ' num2str(N(4))], ['N = ' num2str(N(5))], ...
     'interpreter', 'latex', 'FontSize', ...
     10, 'Location', 'southwest');
